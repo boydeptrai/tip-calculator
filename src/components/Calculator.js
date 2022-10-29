74ea7682f303d15e9ccf680ddfa9779864eb7023
Tuan Tran <langthamht047@gmail.com>
2022-10-29 03:35:23 -0700

    map state to props and connect API

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,20 +1,16 @@
 import { Card } from '@twilio-paste/core';
 import React from 'react';
 import {NewItemForm} from './NewItemForm';
-import {MenuItems} from './MenuItems' ;
 import {TipSelectContainer} from '../containers/TipSelectContainer';
 import {Summary} from './Summary'
+import { MenuItemsContainer } from '../containers/MenuItemsContainer';
 
-const items = [
-  { uuid: 1, name: 'Tofu Roast', price: 14, quantity: 1 },
-  { uuid: 2, name: 'Vegan Ham', price: 12, quantity: 1 }
-];
 
 export default function Calculator() {
   return (
     <Card>
       <NewItemForm />
-      <MenuItems items={items}/>
+      <MenuItemsContainer/>
       <TipSelectContainer />
       <Summary />
     </Card>
